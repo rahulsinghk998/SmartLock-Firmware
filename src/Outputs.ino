8ccba085e5155dd3d8079beb82cca038a9cb528b
azhang100 <azhang100@hotmail.com>
2015-04-13 20:28:25 -0500

    Commented for Alina

--- src/Outputs.ino
+++ src/Outputs.ino
@@ -2,14 +2,19 @@
 //===========Pot stuff====================
 
 int potScaling = 1000/360; int potOffset = 0;// in degrees
-int closedPot(){return (closedAngle+potOffset+potBuffer*turnDirection)*potScaling;}
+// for calibrating the analog values we get from the potentiometer to the actual angle of the lock switcher
+int closedPot(){return (closedAngle+potOffset+potBuffer*turnDirection)*potScaling;} 
+// returns the value of the analog value that we should get if the lock is in the closed position.
+// now reading this function again I realized I wrote it wrong...it should not consider potbuffer.  
+//potbuffer should be implemented when comparing the value from the potentiometer to the value this function gives.  Oh well.
 int openPot(){return (openAngle+potOffset-potBuffer*turnDirection)*potScaling;}
+// ditto
 
-int potPin = A0;
+int potPin = A0; // the potentiometer is plugged into the analog 0 pin
 
 //=============actual outputs===================
 
-char getPosition(){
+char getPosition(){ // returns the the position (of the (open, closed, unknown) of the knob
   int sensorValue = analogRead(potPin);
   char pot = 'j';
   if (sensorValue < closedPot()) {pot = 'c';}
@@ -17,6 +22,6 @@ char getPosition(){
   else {pot = 'm';}
   return pot;}
   
-void givePosition(){BLE.println(getPosition());}
+void givePosition(){BLE.println(getPosition());} // sends the position (of the (open, closed, unknown) to bluetooth
 
-void jamAlert(){char text='j'; BLE.println(text);}
+void jamAlert(){char text='j'; BLE.println(text);} // sends a "j" to BLE.  right now it's just for debugging purposes.
