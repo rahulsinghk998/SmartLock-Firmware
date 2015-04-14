d17a12adf40dedaa6cb85b36bd41ea2b7f81a52a
azhang100 <azhang100@hotmail.com>
2015-04-13 20:33:27 -0500

    Commented for Alina

--- src/memory.ino
+++ src/memory.ino
@@ -2,6 +2,20 @@
 #include <EEPROM.h>
 boolean restartOverride = false; // for debugging purposes, should normally be false
 
+// use of EEPROM is as follows:
+// EEPROM.write(123,234) writes the byte 234 to the address 123
+// EEPROM.read(123) returns the byte stored at address 123
+// you should be able to figure the rest from there.
+// It seems like this should be hardcoded since there's no way to edit them, but 
+// there is additional code I will have you write in the future that actually edits these values.
+
+void startupSettings(){
+  readSettings(); // I used readSettings twice, which is expensive (a couple seconds), 
+  // but you only activate this function when you first turn
+  // it on so for now it's acceptable.
+  if ((settingsSet == 0) || (restartOverride)) {writeDefault();readSettings();}
+} // settingsSet should really be a boolean.  The problem is that I'm writing it in EEPROM, which only stores bytes.
+
 int addressSettingsSet = 0;
 int addressOpenAngle = 1;
 int addressClosedAngle = 2;
@@ -27,8 +41,3 @@ void readSettings(){
   potBuffer = EEPROM.read(addressPotBuffer);
   turnDirection = EEPROM.read(addressTurnDirection);
   power = EEPROM.read(addressPower);}
-
-void startupSettings(){
-  readSettings();
-  if ((settingsSet == 0) || (restartOverride)) {writeDefault();readSettings();}
-}
