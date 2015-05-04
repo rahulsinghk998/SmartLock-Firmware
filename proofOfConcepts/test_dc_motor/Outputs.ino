2e66f31b51ac29245eafada9ca1edf4000f6af62
azhang100 <azhang100@hotmail.com>
2015-05-03 23:44:27 -0500

    code is there to test dc motor
    can now turn dc motor a little bit using "r" and "l"

---
+++ proofOfConcepts/test_dc_motor/Outputs.ino
@@ -0,0 +1,44 @@
+
+// ===== generic =========
+
+char getPosition(){ // returns the the position (of the (open, closed, unknown) of the knob
+  char pot = 'j';
+  int angle=getAngle()
+  if (angle < closedPot()) {pot = 'c';}
+  else if (angle > openPot()) {pot = 'o';}
+  else {pot = 'm';}
+  return pot;}
+  
+int closedPot(){return (closedAngle+potBuffer*turnDirection)} 
+// returns the angle of the lock for it to be considered "closed"
+int openPot(){return (openAngle-potBuffer*turnDirection)}
+// ditto
+
+void givePosition(){BLE.println(getPosition());} // sends the position (of the (open, closed, unknown) to bluetooth
+
+void giveAngle(){BLE.println(getAngle());}
+
+void jamAlert(){char text='j'; BLE.println(text);} // sends a "j" to BLE.  right now it's just for debugging purposes.
+
+// =========== accelerometer ===============
+
+/*
+          int getAngle(){// returns angle relative to positive x-axis, where z axis is pointing out of the door and positive y-axis is pointing up (away from gravity)
+            int angle = 42;
+            return angle;}
+*/
+
+//out dated
+
+          //===========Pot stuff====================
+          
+          int potScaling = 360/1000;
+          int potOffset = 0;// in degrees
+          // for calibrating the analog values we get from the potentiometer to the actual angle of the lock switcher
+          
+          int potPin = A0; // the potentiometer is plugged into the analog 0 pin
+          
+          int getAngle(){
+            int sensorValue = analogRead(potPin);
+            return (sensorValue)*potScaling potOffset
+          }
---
+++ proofOfConcepts/test_dc_motor/memory.ino
@@ -0,0 +1,43 @@
+
+#include <EEPROM.h>
+boolean restartOverride = false; // for debugging purposes, should normally be false
+
+// use of EEPROM is as follows:
+// EEPROM.write(123,234) writes the byte 234 to the address 123
+// EEPROM.read(123) returns the byte stored at address 123
+// you should be able to figure the rest from there.
+// It seems like this should be hardcoded since there's no way to edit them, but 
+// there is additional code I will have you write in the future that actually edits these values.
+
+void startupSettings(){
+  readSettings(); // I used readSettings twice, which is expensive (a couple seconds), 
+  // but you only activate this function when you first turn
+  // it on so for now it's acceptable.
+  if ((settingsSet == 0) || (restartOverride)) {writeDefault();readSettings();}
+} // settingsSet should really be a boolean.  The problem is that I'm writing it in EEPROM, which only stores bytes.
+
+int addressSettingsSet = 0;
+int addressOpenAngle = 1;
+int addressClosedAngle = 2;
+int addressPotBuffer = 3; // the tolerance of the potentiometer
+int addressTurnDirection = 4; // 1 or -1
+int addressPower = 5;
+
+void writeDefault(){
+  EEPROM.write(addressSettingsSet, 1);
+  EEPROM.write(addressOpenAngle, 15);
+  EEPROM.write(addressClosedAngle, 170);
+  EEPROM.write(addressPotBuffer, 10);
+  EEPROM.write(addressTurnDirection, 1);
+  EEPROM.write(addressPower, 255);
+}
+      
+void wipeMemory(){EEPROM.write(addressSettingsSet, 0);} // function not being used atm
+
+void readSettings(){
+  settingsSet = EEPROM.read(addressSettingsSet);
+  openAngle = EEPROM.read(addressOpenAngle);
+  closedAngle = EEPROM.read(addressClosedAngle);
+  potBuffer = EEPROM.read(addressPotBuffer);
+  turnDirection = EEPROM.read(addressTurnDirection);
+  power = EEPROM.read(addressPower);}
